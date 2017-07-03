282fe1ac7f371afa57c9d9eb0fd808f698f6b76d
Adib <pratama.adib@gmail.com>
2017-07-03 10:21:45 +0700

    Add tray icon and refactor open setting so it could be called from within

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 'use strict';
 
-const {electron, BrowserWindow, app, ipcMain, globalShortcut} = require ('electron')
+const {electron, BrowserWindow, app, ipcMain, globalShortcut, Menu, Tray} = require ('electron')
 
 var mainWindow = null;
 var settingWindow = null;
+var trayIcon = null;
 const configuration = require ('./configuration')
+var trayTemplate = [
+  {
+    label: "Sound Mahchine",
+    enabled: "false"
+  },
+  {
+    label : "Setting",
+    click: openSettingWindow
+  },
+  {
+    label: 'Quit',
+    click: function () {
+      app.quit ()
+    }
+  }
+]
 
 app.on('ready', function() {
     mainWindow = new BrowserWindow({
@@ -22,6 +39,9 @@ app.on('ready', function() {
 
     setGlobalShortcut ();
 
+    trayIcon = new Tray (__dirname + '/app/img/tray-icon-alt.png');
+    var trayMenu = Menu.buildFromTemplate (trayTemplate);
+    trayIcon.setContextMenu (trayMenu);
 });
 
 // listener to close-main-window signal
@@ -30,7 +50,36 @@ ipcMain.on ('close-main-window', function () {
 });
 
 // listener to open-settings-window signal
-ipcMain.on ('open-settings-window', function () {
+ipcMain.on ('open-settings-window', openSettingWindow);
+
+
+ipcMain.on ('close-settings-window', function () {
+  if (settingWindow ) {
+    settingWindow.close ();
+  }
+});
+
+ipcMain.on ('set-global-shortcuts', setGlobalShortcut);
+
+
+function setGlobalShortcut () {
+  globalShortcut.unregisterAll ();
+
+  var shortcutKeySetting = configuration.readSetting ('shortcutKeys');
+  var shortcutPrefix = shortcutKeySetting.length == 0 ? "" : shortcutKeySetting.join ('+') + '+';
+
+  // bind prefix+1 to play sound 1
+  globalShortcut.register (shortcutPrefix + '1', () => {
+    mainWindow.webContents.send ('global-shortcut', 1); 
+  });
+
+  // bind prefix+2 to play sound 2
+  globalShortcut.register (shortcutPrefix + '2', () => {
+    mainWindow.webContents.send ('global-shortcut', 2); 
+  });
+}
+
+function openSettingWindow () {
   if (settingWindow) {
     return;
   }
@@ -48,30 +97,5 @@ ipcMain.on ('open-settings-window', function () {
   // it to delete variable
   settingWindow.on ('closed', function () {
     settingWindow = null;
-  });
-});
-
-ipcMain.on ('close-settings-window', function () {
-  if (settingWindow ) {
-    settingWindow.close ();
-  }
-});
-
-ipcMain.on ('set-global-shortcuts', setGlobalShortcut);
-
-function setGlobalShortcut () {
-  globalShortcut.unregisterAll ();
-
-  var shortcutKeySetting = configuration.readSetting ('shortcutKeys');
-  var shortcutPrefix = shortcutKeySetting.length == 0 ? "" : shortcutKeySetting.join ('+') + '+';
-
-  // bind prefix+1 to play sound 1
-  globalShortcut.register (shortcutPrefix + '1', () => {
-    mainWindow.webContents.send ('global-shortcut', 1); 
-  });
-
-  // bind prefix+2 to play sound 2
-  globalShortcut.register (shortcutPrefix + '2', () => {
-    mainWindow.webContents.send ('global-shortcut', 2); 
   });
 }
